adf243be19b7692382f382eddc5c511c1e048807
Vegadrone <spenaantonino@gmail.com>
2022-06-23 15:59:30 +0200

    Update: grid in html draft.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 // * L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // * Ogni cella ha un numero progressivo, da 1 a 100.
 // * Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// * Quando l'utente clicca su ogni cella, la cella cliccata 
+// * Quando l'utente clicca su ogni cella, la cella cliccata
 // * si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 //TODO Bonus
@@ -17,7 +17,5 @@
 // * Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 // * Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
+//! ################################## ESERCIZIO ##################################
 
-
-
-alert ('ok')
